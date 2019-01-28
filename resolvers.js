e17c868d269f906a66c71180fdc75e4fb882188d
Anthony <ajssalemo@gmail.com>
2019-01-28 15:03:55 -0500

    updated schema and resolvers

--- resolvers.js
+++ resolvers.js
@@ -1,5 +1,16 @@
 export default {
     Query: {
-        hello: (parent, arg, { models }) => 'hi'
+        getUser: (parent, { username }, { models }) => 
+            models.User.findOne({
+                where: {
+                    username
+                }
+            }),
+    },
+
+    Mutation: {
+        createUser: (parent, args, { models }) => models.User.create(args),
+        updateUser: (parent, args, { models }) => models.User.update(args),
+        deleteUser: (parent, args, { models }) => models.User.destroy(args)
     }
 };
--- schema.js
+++ schema.js
@@ -1,5 +1,18 @@
 export default `
+    type User {
+        username: String!
+        id: String!
+        createdAt: String!
+        updatedAt: String!
+    }
+
     type Query {
-        hello: String!
+        getUser(username: String!): User
+    }
+
+    type Mutation {
+        createUser(username: String!): User
+        updateUser(username: String!): [Int!]!
+        deleteUser(username: String!): Int!
     }
 `;
